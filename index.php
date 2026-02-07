95bee5e7c14bb3b6e1d850f3abfde93e84954306
kaleprobles <kalep.guerrero.robles@gmail.com>
2026-02-07 15:43:40 -0600

    Agrego templates front-page y page
    Se agregan front-page.php, page.php y ajustes en index/style

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
-<main style="padding:40px;font-family:Arial;">
-  <h1>Hola, capitán ✅</h1>
-  <p>Tu tema ya está activo y funcionando.</p>
+
+<main style="padding:40px;">
+  <h1>Index template</h1>
+  <p>Este archivo se usa como respaldo si no existe un template más específico.</p>
 </main>
+
 <?php get_footer(); ?>
